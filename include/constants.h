2c122d1f0e82d6a4fd8c5fa77cd03d9c23e160a7
= <ssh.xero@gmail.com>
2025-03-13 00:22:21 +0100

    feat(minor): Write and read all utility functions

---
+++ include/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define K_MAX_MSG 4096
+#define K_MIN_MSG 1
+
+#endif // !CONSTANTS_H
--- include/utils.h
+++ include/utils.h
@@ -1,7 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stddef.h>
+#include <stdint.h>
+
 void msg(const char *msg);
 void die(const char *msg);
+int32_t read_full(int fd, char *buf, size_t n);
+int32_t write_all(int fd, const char *buf, size_t n);
 
 #endif // UTILS_H
