a3196f1d79a4ce051ad18e2ad5e60b7a52b6cac1
= <ssh.xero@gmail.com>
2025-03-12 19:06:52 +0100

    feat: Listen to and process incoming connections

--- redis.c
+++ redis.c
@@ -1,6 +1,8 @@
 #include <arpa/inet.h>
 #include <errno.h>
+#include <netinet/in.h>
 #include <netinet/ip.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,10 +18,24 @@ static void die(const char *msg) {
     abort();
 }
 
+static void do_something(int connfd) {
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        msg("read() error");
+        return;
+    }
+    printf("client says: %s\n", rbuf);
+
+    char wbuf[] = "world";
+    write(connfd, wbuf, strlen(wbuf));
+}
+
 int main() {
+    // Obtain socket file descriptor
     int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
-        // DIE
+        die("socket()");
     }
 
     int val = 1;
@@ -27,4 +43,32 @@ int main() {
 
     // Bind socket to address
     struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = htonl(0); // wildcard 0.0.0.0
+
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("bind()");
+    }
+
+    // Listen for connections
+    rv = listen(fd, SOMAXCONN);
+    if (rv) {
+        die("listen()");
+    }
+
+    // Process any incoming connections
+    while (true) {
+        struct sockaddr_in client_addr = {};
+        socklen_t socklen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &socklen);
+        if (connfd < 0) {
+            continue; // error
+        }
+        do_something(connfd);
+        close(connfd);
+    }
+
+    return 0;
 }
